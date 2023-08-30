8bfdc5e92162d19ae3e8ccdb96537edc0fcf9d32
Mustafa Jamshidi <mustafa_jamis1@yahoo.com>
2023-08-30 02:16:10 -0500

    adding the snake c++ game

---
+++ SnakeGameC++/Snake.h
@@ -0,0 +1,17 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+// Constants
+const int width = 20;
+const int height = 20;
+
+// Function declarations
+void setUp();
+void Draw();
+void Input();
+void Logic();
+#endif
